f274cee2f5f7eeb459d779ed082af3c9b059e229
Geoff Roodt <geoffr@ezitracker.co.nz>
2018-02-02 09:04:35 +1300

    Added nav bar

--- redux-chat/src/app/chat-nav-bar/chat-nav-bar.component.ts
+++ redux-chat/src/app/chat-nav-bar/chat-nav-bar.component.ts
@@ -1,13 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import {AppStore} from '../app.store';
+import * as Redux from 'redux';
+import {AppState, getUnreadMessagesCount} from '../app.reducer';
 
 @Component({
   selector: 'chat-nav-bar',
   templateUrl: './chat-nav-bar.component.html',
   styleUrls: ['./chat-nav-bar.component.css']
 })
-export class ChatNavBarComponent implements OnInit {
 
-  constructor() { }
+export class ChatNavBarComponent {
+  unreadMessagesCount: number;
+
+  constructor(@Inject(AppStore) private store: Redux.Store<AppState>) {
+    store.subscribe(() => this.updateState());
+    this.updateState();
+  }
+
+  updateState(){
+    this.undreadMessagesCount = getUnreadMessagesCount(this.store.getState());
+  }
 
   ngOnInit() {
   }
