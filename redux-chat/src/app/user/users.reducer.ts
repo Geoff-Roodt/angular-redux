003a52db5644d27963bbbc43b8a1e27ade91b179
Geoff Roodt <geoffr@ezitracker.co.nz>
2018-02-01 16:49:33 +1300

    imported users reducer

--- redux-chat/src/app/user/users.reducer.ts
+++ redux-chat/src/app/user/users.reducer.ts
@@ -1,3 +1,8 @@
+import { Action } from 'redux';
+import { User } from './user.model';
+import * as UserActions from './user.actions';
+import { createSelector } from 'reselect';
+
 export interface UsersState{
   currentUser: User;
 }
