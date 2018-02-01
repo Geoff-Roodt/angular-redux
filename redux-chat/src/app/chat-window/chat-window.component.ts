2214347f19f6ff9e7d3290fad2bbf61b2a1372b9
Geoff Roodt <geoffr@ezitracker.co.nz>
2018-02-02 09:52:35 +1300

    Added Chat window code and html

--- redux-chat/src/app/chat-window/chat-window.component.ts
+++ redux-chat/src/app/chat-window/chat-window.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, ElementRef } from '@angular/core';
+import * as Redux from 'redux';
+import {AppStore} from '../app.store';
+import {AppState, getCurrentThread, getCurrentUser} from '../app.reducer';
+import {User} from '../user/user.model';
+import {Thread} from '../thread/thread.model';
+import * as ThreadActions from '../thread/thread.actions';
 
 @Component({
   selector: 'chat-window',
   templateUrl: './chat-window.component.html',
   styleUrls: ['./chat-window.component.css']
 })
-export class ChatWindowComponent implements OnInit {
+export class ChatWindowComponent {
+  currentThread: Thread;
+  draftMessage: {text:string};
+  currentUser: User;
 
-  constructor() { }
+  constructor(@Inject(AppStore) private store: Redux.Store<AppState>, private el: ElementRef) {
+    store.subscribe(() => this.updateState());
+    this.updateState();
+    this.draftMessage = {text: ''};
+  }
 
-  ngOnInit() {
+  updateState(){
+    const state = this.store.getState();
+    this.currentThread = getCurrentThread(state);
+    this.user = getCurrentUser(state);
+    this.scrollToBottom();
+  }
+
+  scrollToBottom(): void{
+    const scrollPane: any = this.el.nativeElement.querySelector('.msg-container-base');
+    if (scrollPane){
+      setTimeout(() => scrollPane.scrollTop = scrollPane.scrollHeight);
+    }
+  }
+
+  sendMessage():void{
+    this.store.dispatch(ThreadActions.addMessage(this.currentThread, {
+      author: this.currentUser,
+      isRead: true,
+      text: this.draftMessage.text
+    }));
+    this.draftMessage = {text:''};
+  }
+
+  onEnter(event: any):void{
+    this.sendMessage();
+    event.preventDefault();
   }
 
 }
