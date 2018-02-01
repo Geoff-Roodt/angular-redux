857e4e8c197dff663e16f10a132f35693661ec49
Geoff Roodt <geoffr@ezitracker.co.nz>
2018-02-01 13:59:31 +1300

    Added 3rd tutorial

---
+++ redux-chat/tutorial/03-adjusting-reducer-switch.ts
@@ -0,0 +1,27 @@
+interface Action{
+  type:string;
+  payload?: any;
+}
+
+interface Reducer<T>{
+  (state:T, action:Action):T;
+}
+
+let incrementAction: Action = {type:'INCREMENT'};
+let decrementAction: Action = {type:'DECREMENT'};
+
+let reducer: Reducer<number> = (state:number, action:Action) => {
+  switch(action.type){
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+
+console.log(reducer(0, incrementAction));
+console.log(reducer(1, decrementAction));
+console.log(reducer(100, 'UNKNOWN'));
