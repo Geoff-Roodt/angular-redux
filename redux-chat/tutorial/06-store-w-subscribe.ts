65719acf49433fedd70e0a4fa9c9e1ec332d90a4
Geoff Roodt <geoffr@ezitracker.co.nz>
2018-02-01 14:23:09 +1300

    Tutorial 6.1

---
+++ redux-chat/tutorial/06-store-w-subscribe.ts
@@ -0,0 +1,54 @@
+interface Action {
+  type: string;
+  payload?: any;
+}
+
+interface Reducer<T> {
+  (state: T, action: Action): T;
+}
+
+interface ListenerCallback {
+  (): void;
+}
+
+interface UnsubscribeCallback{
+  ():void;
+}
+
+class Store<T>{
+  private _state:T;
+  private _listeners: ListenerCallback[] = [];
+
+  constructor(private reducer:Reducer<T>, initialState:T){
+    this._state = initialState;
+  }
+
+  getState():T{
+    return this._state;
+  }
+
+  dispatch(action:Action):void{
+    this._state = this.reducer(this._state, action);
+    this._listeners.forEach((listener: ListenerCallback) => listener());
+  }
+
+  subscribe(listener:ListenerCallback): UnsubscribeCallback{
+    this._listeners.push(listener);
+    return () => {this._listeners = this._listeners.filter(x => x !== listener);};
+  }
+}
+
+let store = new Store<number>(reducer, 0);
+console.log(store.getState());
+
+let unsubscribe = store.subscribe(() => {
+  console.log('subscribed: ', store.getState());
+});
+
+store.dispatch({type: 'INCREMENT'});
+store.dispatch({type: 'INCREMENT'});
+
+unsubscribe();
+store.dispatch({type: 'DECREMENT'});
+
+console.log(store.getState());
