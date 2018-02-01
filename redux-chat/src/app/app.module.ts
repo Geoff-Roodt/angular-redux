0de5327a956d207dd4c77567e4dce154d3d4e356
Geoff Roodt <geoffr@ezitracker.co.nz>
2018-02-02 08:44:54 +1300

    Bundles app store

--- redux-chat/src/app/app.module.ts
+++ redux-chat/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
+import {AppState, default as reducer} from './app.reducer';
 
 import { AppComponent } from './app.component';
+import {FromNowPipe} from './pipes/from-now.pipe';
 
+import {AppStore, appStoreProviders} from './app.store';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FromNowPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    appStoreProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ redux-chat/src/app/app.store.ts
@@ -0,0 +1,15 @@
+import {InjectionToken} from '@angular/core';
+import {createStore, Store, compose, StoreEnhancer} from 'redux';
+import {AppState, default as reducer} from './app.reducer';
+
+export const AppStore = new InjectionToken('App.store');
+
+const devtools: StoreEnhancer<AppState> = window['devToolExtension'] ? window['devToolExtension']() : f => f;
+
+export function createAppStore(): Store<AppState>{
+  return createStore<AppState>(reducer, compose(devtools));
+}
+
+export const appStoreProviders = [
+  {provide: AppStore, useFactory: createAppStore}
+];
