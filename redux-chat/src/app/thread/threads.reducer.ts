52e8b95136f77690c8063cad03af947ddfc68691
Geoff Roodt <geoffr@ezitracker.co.nz>
2018-02-01 16:59:09 +1300

    Thread reducer

--- redux-chat/src/app/thread/threads.reducer.ts
+++ redux-chat/src/app/thread/threads.reducer.ts
@@ -1,3 +1,10 @@
+import { Action } from 'redux';
+import { createSelector } from 'reselect';
+
+import { Thread } from './thread.model';
+import { Message } from '../message/message.model';
+import * as ThreadActions from './thread.actions';
+
 export interface ThreadsEntities{
   [id:string]: Thread;
 }
@@ -13,3 +20,24 @@ const initialState: ThreadState = {
   currentThreadId: null,
   entities: {}
 };
+
+export const ThreadsReducer = function(state: ThreadsState = initialState, action: Action): ThreadsState{
+  switch(action.type){
+    case ThreadActions.ADD_THREAD: {
+      const thread = (<ThreadActions.AddThreadAction>action).thread;
+
+      if (state.ids.includes(thread.id)){
+        return state;
+      }
+
+      return {
+        ids: [...state.ids, thread.id],
+        currentThreadId: state.currentThreadId,
+        entities: Object.assign({}, state.entities, {[thread.id]: thread})
+      };
+    }
+
+    default:
+      return state;
+  }
+}
