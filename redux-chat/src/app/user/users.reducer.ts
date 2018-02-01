c9139755c84063623c4c29a0845f8d02bb3f292c
Geoff Roodt <geoffr@ezitracker.co.nz>
2018-02-01 16:52:27 +1300

    More reducer code

--- redux-chat/src/app/user/users.reducer.ts
+++ redux-chat/src/app/user/users.reducer.ts
@@ -10,3 +10,14 @@ export interface UsersState{
 const initialState: UsersState = {
   currentUser: null
 };
+
+export const UsersReducer = function(state: UsersState = initialState, action: Action): UsersState{
+  switch (action.type){
+    case UserActions.SET_CURRENT_USER:
+      const user:User = (<UserActions.SetCurrentUserAction>action).user;
+      return { currentUser: user; };
+
+    default:
+      return state;
+  }
+};
