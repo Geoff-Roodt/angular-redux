abffba93bc895b61af001561586c0840a796d311
Geoff Roodt <geoffr@ezitracker.co.nz>
2018-02-02 08:55:00 +1300

    Added extra base components

--- redux-chat/src/app/app.module.ts
+++ redux-chat/src/app/app.module.ts
@@ -10,12 +10,20 @@ import {FromNowPipe} from './pipes/from-now.pipe';
 
 import {AppStore, appStoreProviders} from './app.store';
 import { ChatPageComponent } from './chat-page/chat-page.component';
+import { ChatNavBarComponent } from './chat-nav-bar/chat-nav-bar.component';
+import { ChatThreadsComponent } from './chat-threads/chat-threads.component';
+import { ChatThreadComponent } from './chat-thread/chat-thread.component';
+import { ChatWindowComponent } from './chat-window/chat-window.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FromNowPipe,
-    ChatPageComponent
+    ChatPageComponent,
+    ChatNavBarComponent,
+    ChatThreadsComponent,
+    ChatThreadComponent,
+    ChatWindowComponent
   ],
   imports: [
     BrowserModule,
